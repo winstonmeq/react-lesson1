e0766f4750ef9f137751c3b26604042a0177136e
winstonmeq <winstonmequila@gmail.com>
2024-08-20 08:56:28 +0800

    adding props in the component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,10 @@
 
 
-function Todo() {
+function Todo(props) {
 
     return (
         <div className="card">
-        <h2>Title</h2>
+        <h2>{props.text}</h2>
         <div className="actions">
           <button className="btn">Delete</button>
         </div>
